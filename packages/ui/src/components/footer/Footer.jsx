560eb9b77041b819992864e176e13b1f37c49557
Shafat Hussain <121899434+shafat730@users.noreply.github.com>
2025-02-16 00:17:21 +0530

    Update Footer.jsx

--- packages/ui/src/components/footer/Footer.jsx
+++ packages/ui/src/components/footer/Footer.jsx
@@ -28,7 +28,7 @@ function Footer() {
             rel="noopener noreferrer"
             className={styles["footer-powered"]}
           >
-            Powered by TeamShik
+          
           </a>
         </div>
       </footer>
