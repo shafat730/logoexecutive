6015aee5bf35bac5b32af3e0173fc3ffe79cdbad
Shafat Hussain <121899434+shafat730@users.noreply.github.com>
2025-02-17 17:29:46 +0530

    Update logo.js

--- packages/app/controllers/logo.js
+++ packages/app/controllers/logo.js
@@ -73,61 +73,61 @@ async function getLogoController(req, res, next) {
  * Validates input, checks subscription limits, fetches matching companies and their logos.
  * Responds with a list of logos.
  */
-// async function searchLogoController(req, res, next) {
-//   try {
-//     const imageServices = new ImageService();
-//     const keyService = new KeyService();
-//     const subscriptionService = new SubscriptionService();
+async function searchLogoController(req, res, next) {
+  try {
+    const imageServices = new ImageService();
+    const keyService = new KeyService();
+    const subscriptionService = new SubscriptionService();
 
-//     const { error, value } = getSearchQuerySchema.validate(req.query);
-//     if (error) {
-//       return res.status(422).json({
-//         message: error.message,
-//         statusCode: 422,
-//         error: STATUS_CODES[422],
-//       });
-//     }
-//     const { API_KEY, companyNameBeginsWith } = value;
-//     const key = await keyService.getApiKey(API_KEY);
-//     if (!key) {
-//       return res.status(403).json({
-//         message: "Invalid API KEY.",
-//         error: STATUS_CODES[403],
-//         statusCode: 403,
-//       });
-//     }
+    const { error, value } = getSearchQuerySchema.validate(req.query);
+    if (error) {
+      return res.status(422).json({
+        message: error.message,
+        statusCode: 422,
+        error: STATUS_CODES[422],
+      });
+    }
+    const { API_KEY, companyNameBeginsWith } = value;
+    const key = await keyService.getApiKey(API_KEY);
+    if (!key) {
+      return res.status(403).json({
+        message: "Invalid API KEY.",
+        error: STATUS_CODES[403],
+        statusCode: 403,
+      });
+    }
 
-//     const subscription = await subscriptionService.getSubscription(
-//       key.subscription_id
-//     );
-//     if (subscription.usage_count >= subscription.usage_limit) {
-//       return res.status(403).json({
-//         message: "Subscription Limit reached. Consider upgrading your plan",
-//         error: STATUS_CODES[403],
-//         statusCode: 403,
-//       });
-//     }
+    const subscription = await subscriptionService.getSubscription(
+      key.subscription_id
+    );
+    if (subscription.usage_count >= subscription.usage_limit) {
+      return res.status(403).json({
+        message: "Subscription Limit reached. Consider upgrading your plan",
+        error: STATUS_CODES[403],
+        statusCode: 403,
+      });
+    }
 
-//     const regexPattern = new RegExp(`^${companyNameBeginsWith}`, "i");
-//     const companyList = await imageServices.fetchCompanyList(regexPattern);
-//     if (companyList.length === 0) {
-//       return res.status(404).json({
-//         message: "No companies found matching the provided domain key.",
-//         statusCode: 404,
-//         error: STATUS_CODES[404],
-//       });
-//     }
+    const regexPattern = new RegExp(`^${companyNameBeginsWith}`, "i");
+    const companyList = await imageServices.fetchCompanyList(regexPattern);
+    if (companyList.length === 0) {
+      return res.status(404).json({
+        message: "No companies found matching the provided domain key.",
+        statusCode: 404,
+        error: STATUS_CODES[404],
+      });
+    }
 
-//     const dataList = await imageServices.getDataList(companyList);
-//     await subscriptionService.incrementUsageCount(subscription);
+    const dataList = await imageServices.getDataList(companyList);
+    await subscriptionService.incrementUsageCount(subscription);
 
-//     return res.status(200).json({
-//       statusCode: 200,
-//       data: dataList,
-//     });
-//   } catch (err) {
-//     next(err);
-//   }
-// }
+    return res.status(200).json({
+      statusCode: 200,
+      data: dataList,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
 
 module.exports = { getLogoController, searchLogoController };
