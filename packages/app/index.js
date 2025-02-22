3e9b2b1a84c42f7863fe25166c462074778f6cad
Shafat Hussain <121899434+shafat730@users.noreply.github.com>
2025-02-22 15:08:42 +0530

    Update index.js

--- packages/app/index.js
+++ packages/app/index.js
@@ -33,7 +33,7 @@ const app = express();
 app.use(cookieParser());
 app.disable("x-powered-by");
 app.use(express.json());
-app.get("/", (req, res) => res.send("Welcome to the API"));
+app.get("/", (req, res) => res.send("Welcome to the "));
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/", routes);
 
